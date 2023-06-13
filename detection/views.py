b11f9f42da050b43e8cab9d619730bf0633ef24a
DjangodevArif <arifanytest@gmail.com>
2023-06-13 19:09:57 +0600

    root api initialize

--- detection/views.py
+++ detection/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view()
+def index(request):
+    return Response("Hello, world. You're at the main page")
