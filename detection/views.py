de0dc05de0835b7580fc4762421fc576d7114f35
DjangodevArif <arifanytest@gmail.com>
2023-06-14 11:36:24 +0600

    remove comment line

--- detection/views.py
+++ detection/views.py
@@ -28,7 +28,6 @@ class DetectImage(APIView):
         if serialize.is_valid(raise_exception=True):
             data = serialize.validated_data.get("file")
             unique = str(uuid.uuid4()).split("-")[0]
-            # image = Image.open(BytesIO(data.file.read()))
             image_array = np.frombuffer(data.file.read(), np.uint8)
             image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
             model = YOLO('yolov8n.pt')
@@ -36,5 +35,4 @@ class DetectImage(APIView):
             results = model(image, save=True,
                             project="detect", name=file_name)
             detect_image = "detect/"+file_name+"/image0.jpg"
-        # return Response(image, content_type="image/jpg")
         return FileResponse(open(detect_image, "rb"), as_attachment=False)
